6e7770d918770a8a8bdc2ecd8e25b8930f812b44
Ignacio Barrau <ibarrau@users.noreply.github.com>
2020-10-23 14:31:44 -0300

    testinggggggggg

--- Bi/Resoluciones/MomentoNerdGit.sql
+++ Bi/Resoluciones/MomentoNerdGit.sql
@@ -4,8 +4,4 @@
 ----------------------------------------- */
 
 SELECT * FROM Tabla1
-<<<<<<< Updated upstream
 WHERE Columna1 IN ('Activo', 'Iniciado', 'Cerrado')
-=======
-WHERE Columna1 IN ('Activo', 'Presentado')
->>>>>>> Stashed changes
